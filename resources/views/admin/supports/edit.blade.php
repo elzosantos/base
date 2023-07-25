ee1940cc1b9ee767daa373708aa58a05c42fbd79
Elzo Santos <elzo.santos.sousa@gmail.com>
2023-07-24 22:16:15 -0300

    UX UI - Add

--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -1,8 +1,14 @@
-<h1>Duvida - {{ $support->id }}</h1>
+@extends('admin.layouts.app')
 
-<x-alert/>
+@section('title', 'Editar Tópico')
 
+@section('header')
+<h1 class="text-lg text-black-500"  style="color: rgb(255, 255, 255); ">Duvida - {{ $support->id }}</h1>
+@endsection
+
+@section('content')
 <form action="{{ route('supports.update', $support->id) }}" method="POST">
     @method('PUT')
-    @include('admin.partials.form', ['support' => $support])
+    @include('admin.supports.partials.form', ['support' => $support])
 </form>
+@endsection
--- resources/views/admin/supports/show.blade.php
+++ resources/views/admin/supports/show.blade.php
@@ -1,16 +1,19 @@
-<h1>
-    Detalhes da Duvida
-</h1>
+ 
 
-<ul>
-    <li>Assunto: {{ $support->subject }}</li>
-    <li>Status: {{ $support->status }}</li>
-    <li>Descricao: {{ $support->body }}</li>
+@extends('admin.layouts.app')
 
-</ul>
+@section('title', 'Detalhes da Duvida')
+
+@section('header')
+<h1 class="text-lg text-black-500"  style="color: rgb(255, 255, 255); ">Detalhes Duvida - {{ $support->id }}</h1>
+@endsection
+
+@section('content')
+
 
 <form action="{{ route('supports.destroy', $support->id )}}" method="POST">
     @csrf()
     @method('DELETE') 
         <button type="submit">Deletar</button>
 </form>
+@endsection
